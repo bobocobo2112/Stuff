d827ccd0f9cae59a6aac654a193832a3f3e72152
1205765 <bobocobo2112@gmail.com>
2015-10-19 15:28:16 +0100

    stuff

--- index.php
+++ index.php
@@ -3,23 +3,30 @@
     <head>
         </head>
 
+    <body>
+    Welcome <?php echo $_GET["name"]; ?><br>
+    Your email address is: <?php echo $_GET["email"]; ?>
     <p>
         <?php
 
-        echo $_GET["name"];
-        $name = "Edgar";
-        $myAge = 16;
+        //echo $_GET["name"];
 
-        if ($myAge == 16) {
-            print "glasses";
-            echo "sup";
-        }
-        else if ($myAge == 18){
-        print "Mugs";
-        }
-        else if ($myAge == 21){
-            print "sausage roll";
-        }
+
+
+
+        /*       $name = "Edgar";
+               $myAge = 16;
+
+               if ($myAge == 16) {
+                   print "glasses";
+                   echo "sup";
+               }
+               else if ($myAge == 18){
+               print "Mugs";
+               }
+               else if ($myAge == 21){
+                   print "sausage roll";
+               }*/
         ?>
     </p>
     </body>
