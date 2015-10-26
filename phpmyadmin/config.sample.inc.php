62d7a7dbc6d010b2600c54dbaf59dd84e5a2f993
1205765 <bobocobo2112@gmail.com>
2015-10-26 15:13:27 +0000

    sup mate

--- phpmyadmin/config.sample.inc.php
+++ phpmyadmin/config.sample.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'eu-cdbr-azure-west-c.cloudapp.net';
+$cfg['Servers'][$i]['host'] = 'eu-cdbr-azure-north-d.cloudapp.net';
 $cfg['Servers'][$i]['connect_type'] = 'tcp';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
